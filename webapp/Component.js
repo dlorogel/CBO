63a60dadac13e49412a6b74fa656a7012e753a60
Riccardo Rossi <rrossi@deloitte.it>
2022-07-20 09:44:08 +0000

    first commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("it.orogel.zz1ora2.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
